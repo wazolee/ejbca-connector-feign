48c381f687fe667725c2bf311a35b0b636ab3695
ZV <wazolee@gmail.com>
2025-05-04 21:54:08 +0200

    init spring boot

---
+++ src/main/java/com/learn/ejbcaconnectorfeign/EjbcaConnectorFeignApplication.java
@@ -0,0 +1,13 @@
+package com.learn.ejbcaconnectorfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjbcaConnectorFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjbcaConnectorFeignApplication.class, args);
+    }
+
+}
